66446526f1653436eb36d5307aca04e9df6ca582
Steve Townend <steve@townendmail.co.uk>
2025-11-28 15:17:15 +0000

    Added time column to matches

--- src/app/models/displayed-columns-matches.ts
+++ src/app/models/displayed-columns-matches.ts
@@ -1,15 +1,19 @@
 export class DisplayedColumnsForMatches {
     date: [on: boolean, order: number] = [true, 0];
-    day: [on: boolean, order: number] = [true, 1];
-    description: [on: boolean, order: number] = [true, 2];
-    cup: [on: boolean, order: number] = [true, 3];
-    number: [on: boolean, order: number] = [true, 4];
-    more: [on: boolean, order: number] = [true, 5];
+    time: [on: boolean, order: number] = [true, 1];
+    day: [on: boolean, order: number] = [true, 2];
+    description: [on: boolean, order: number] = [true, 3];
+    cup: [on: boolean, order: number] = [true, 4];
+    number: [on: boolean, order: number] = [true, 5];
+    more: [on: boolean, order: number] = [true, 6];
 
     public get displayedColumns(): string[] {
         var colsToDisplay: string[] = [];
 
-        if (this.date[0] == true) { colsToDisplay[this.date[1]] = "date"}
+        if (this.date[0] == true) { 
+            colsToDisplay[this.date[1]] = "date"
+            colsToDisplay[this.time[1]] = "time"
+        }
         if (this.day[0] == true) { colsToDisplay[this.day[1]] = "day"}
         if (this.description[0]) { colsToDisplay[this.description[1]] = "description"}
         if (this.cup[0]) { colsToDisplay[this.cup[1]] = "cup"}
@@ -27,6 +31,7 @@ export class DisplayedColumnsForMatches {
 
     public SetAll(): void {
         this.date[0] = true;
+        this.time[0] = true;
         this.day[0] = true;
         this.description[0] = true;
         this.cup[0] = true;
@@ -36,6 +41,7 @@ export class DisplayedColumnsForMatches {
 
     public ClearAll(): void {
         this.date[0] = false;
+        this.time[0] = false;
         this.day[0] = false;
         this.description[0] = false;
         this.cup[0] = false;
--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -10,10 +10,10 @@ export class GlobalService {
     this.OnLocalhost = window.location.href.indexOf("localhost") > 0 || window.location.href.indexOf(".devtunnels.ms") > 0 ? true : false;
 
     // Use this one for normal development
-    //this.ApiUrl = this.OnLocalhost ? "https://localhost:5001" : "https://t5nynu5k43.execute-api.eu-west-1.amazonaws.com/Prod";
+    this.ApiUrl = this.OnLocalhost ? "https://localhost:5001" : "https://t5nynu5k43.execute-api.eu-west-1.amazonaws.com/Prod";
 
     // Use this one with a DevTunnel for Stripe Testing - see OneNote "Stripe TEST/LIVE Modes"
-    this.ApiUrl = this.OnLocalhost ? "https://dpnhbrrn-5001.uks1.devtunnels.ms" : "https://t5nynu5k43.execute-api.eu-west-1.amazonaws.com/Prod";
+    //this.ApiUrl = this.OnLocalhost ? "https://dpnhbrrn-5001.uks1.devtunnels.ms" : "https://t5nynu5k43.execute-api.eu-west-1.amazonaws.com/Prod";
     
     // Use this one with a the Dev deployment for AWS Lambda Testing - see OneNote "Stripe TEST/LIVE Modes"
     //this.ApiUrl = this.OnLocalhost ? "https://2zgwyov4ub.execute-api.eu-west-1.amazonaws.com/Prod" : "https://t5nynu5k43.execute-api.eu-west-1.amazonaws.com/Prod";
